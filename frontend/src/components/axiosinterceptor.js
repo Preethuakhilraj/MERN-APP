a4d11b6b9cc6cc639f835861035f49feef99bf91
Preethuakhilraj <46960642+Preethuakhilraj@users.noreply.github.com>
2024-08-11 12:51:14 +0530

    gitignore updated

---
+++ frontend/src/components/axiosinterceptor.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://ictak-internship-portal-server.vercel.app',
+  // baseURL: 'http://localhost:4000',
+});
+
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
